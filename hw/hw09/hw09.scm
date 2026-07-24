311ac88024073b732c8466c06c759453134d0897
everlasting. <2279839810@qq.com>
2026-07-24 12:28:19 +0800

    finish

--- hw/hw09/hw09.scm
+++ hw/hw09/hw09.scm
@@ -7,13 +7,15 @@
             (curry-cook (cdr formals) body))))
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+    curry
+    (curry-consume (curry (car args)) (cdr args))))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
 
 (define (switch-to-cond switch-expr)
-  (cons _________
+  (cons 'cond
         (map (lambda (option)
-               (cons _______________ (cdr option)))
+               (cons `(equal? ,(car (cdr switch-expr)) ,(car option)) (cdr option)))
              (car (cdr (cdr switch-expr))))))
